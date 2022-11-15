01c4a3399955bfeb269145e8f8ea67812fa71d64
checkthesound <danil.dybko@gmail.com>
2022-11-16 00:24:29 +0300

    start work

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<?php include_once "i/php/head.php" ?>
+<title>Игровой разработчик Creagoo</title>
+
+<body>
+    <?php include_once "i/php/header.php" ?>
+
+    <main class="container">
+        <p>RU:</p>
+        <h1 class="h-ru">Привет, это добрый и открытый разработчик из IT!</h1>
+        <p>
+            Привет, меня зовут Даниил Дыбка, я 3 года развиваюсь как мобильный разработчик.
+            Меня всегда привлекала идея помощи людям, у которых только начинается данный путь.
+            Возможно, к сожалению, но я не могу помочь каждому. Я не могу уледить каждому время.
+            Зато каждый может уделить время моей разработке, встречайте - Технологии!
+        </p>
+        <p>
+            Это проект-помощь тем, кто еще многого не знает, кому предстоит многое узнать,
+            кто достигнет многого и на многое готов!
+        </p>
+
+        <p>EN:</p>
+        <h1 class="h-en">Hello, its Daniil Dybka!</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ab. Officia ratione sapiente corrupti, officiis quas dolores exercitationem ea ullam impedit quisquam possimus dolorum quam totam? Eaque magnam explicabo consequatur!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatum consectetur!
+        </p>
+    </main>
+
+    <?php include_once "i/php/footer.php" ?>
+</body>
+
+</html>
