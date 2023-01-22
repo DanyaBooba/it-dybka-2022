1e7ca983b3ed5d31108eb1c2a83a2e090c35c62d
checkthesound <danil.dybko@gmail.com>
2023-01-22 23:52:18 +0300

    get global its

--- i/php/footer-en.php
+++
@@ -1,5 +0,0 @@
-<footer style="margin-top: 50px">
-    <div class="text-center">
-        <p class="fs-1 h-en" style="color: #cecece">it.dybka.ru</p>
-    </div>
-</footer>
--- i/php/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="margin-top: 50px">
-    <div class="text-center">
-        <p class="fs-1 h-ru" style="color: #cecece">it.dybka.ru</p>
-    </div>
-</footer>
